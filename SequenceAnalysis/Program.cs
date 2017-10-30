cf5a61d2354603e3e4e2ba98de3b3cdc81650d2e
Rodion Girbu <fpsoff@outlook.com>
2017-10-30 19:21:12 +0300

    1.0

--- SequenceAnalysis/Program.cs
+++ SequenceAnalysis/Program.cs
@@ -11,6 +11,7 @@ namespace SequenceAnalysis
     {
         private static Dictionary<char, int> _messageLetterFrequency = new Dictionary<char, int>();
         private static readonly List<char> LetterFrequency = new List<char> { 'о', 'е', 'а', 'и', 'н', 'т', 'с', 'р', 'в', 'л', 'к', 'м', 'д', 'п', 'у', 'я', 'ы', 'ь', 'г', 'з', 'б', 'ч', 'й', 'х', 'ж', 'ш', 'ю', 'ц', 'щ', 'э', 'ф', 'ъ', 'ё' };
+
         static void Main(string[] args)
         {
             try
@@ -20,6 +21,8 @@ namespace SequenceAnalysis
                 message = message.ToLower();
                 Console.Write($"message: {message}");
                 objReader.Close();
+                var decyphered = new char[message.Length];
+                Dictionary<char, bool> txt = new Dictionary<char, bool>();
                 for (var i = 0; i < message.Length; i++)
                 {
                     if (char.IsLetter(message[i]))
@@ -32,15 +35,36 @@ namespace SequenceAnalysis
                             _messageLetterFrequency.Add(message[i], 1);
                         }
                 }
+
                 var index = 0;
                 var sortedLetters = _messageLetterFrequency.OrderBy(item => item.Value).Reverse();
                 Console.WriteLine();
                 foreach (var item in sortedLetters)
                 {
-                    Console.WriteLine($"{item.Key}: {item.Value}");
-                    message = message.Replace(item.Key, LetterFrequency[index++]);
+                    Console.WriteLine($"Example: {item.Key}: {item.Value}/Wikipedia: {LetterFrequency[index]}");
+                    //message = message.Replace(item.Key, LetterFrequency[index++]);
+                    for (var i = 0; i < message.Length; i++)
+                    {
+                        if (char.IsLetter(message[i]))
+                        {
+                            if (message[i] == item.Key)
+                            {
+                                decyphered[i] = LetterFrequency[index];
+                            }
+                        }
+                        else
+                        {
+                            decyphered[i] = message[i];
+                        }
+                    }
+                    index++;
                 }
-                var objWriter = new StreamWriter(@"C:\Users\rgyrbu\source\repos\SequenceAnalysis\Decyphered.txt");
+                message = string.Empty;
+                foreach (var item in decyphered)
+                {
+                    message += item;
+                }
+                var objWriter = new StreamWriter(@"C:\Users\rgyrbu\source\repos\SequenceAnalysis\Decyphered.txt", false);
                 objWriter.WriteLine(message);
                 objWriter.Close();
                 Console.ReadKey();
