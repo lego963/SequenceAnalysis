82737787b672b806341df70adadad796333641f9
Rodion Girbu <fpsoff@outlook.com>
2017-11-06 14:01:11 +0300

    ANTIHYPE

--- SequenceAnalysis/Program.cs
+++ SequenceAnalysis/Program.cs
@@ -2,47 +2,44 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SequenceAnalysis
 {
-    class Program
+    internal static class Program
     {
-        private static Dictionary<char, int> _messageLetterFrequency = new Dictionary<char, int>();
-        private static readonly List<char> LetterFrequency = new List<char> { 'о', 'е', 'а', 'и', 'н', 'т', 'с', 'р', 'в', 'л', 'к', 'м', 'д', 'п', 'у', 'я', 'ы', 'ь', 'г', 'з', 'б', 'ч', 'й', 'х', 'ж', 'ш', 'ю', 'ц', 'щ', 'э', 'ф', 'ъ', 'ё' };
+        private static readonly Dictionary<char, int> MessageLetterFrequency = new Dictionary<char, int>();
+        //private static readonly List<char> LetterFrequency = new List<char> { 'о', 'а', 'т', 'е', 'и', 'н', 'с', 'р', 'к', 'в', 'д', 'л', 'м', 'у', 'п', 'ы', 'я', 'ь', 'х', 'з', 'б', 'ж', 'й', 'ш', 'г', 'ч', 'ю', 'э', 'щ', 'ц', 'ф', 'ё', 'ъ' };
+        //Даниил
 
-        static void Main(string[] args)
+        private static readonly List<char> LetterFrequency = new List<char> { 'о', 'т', 'е', 'и', 'н', 'а', 'с', 'р', 'в', 'л', 'п', 'к', 'д', 'м', 'ы', 'у', 'ь', 'я', 'г', 'б', 'з', 'й', 'ч', 'ж', 'х', 'ц', 'ю', 'э', 'щ', 'ш', 'ф', 'ё', 'ъ' };
+        //Кс_Наташк
+        private static void Main()
         {
             try
             {
                 var objReader = new StreamReader(@"C:\Users\rgyrbu\source\repos\SequenceAnalysis\message.txt");
                 var message = objReader.ReadLine();
+                // ReSharper disable once PossibleNullReferenceException
                 message = message.ToLower();
-                Console.Write($"message: {message}");
                 objReader.Close();
                 var decyphered = new char[message.Length];
-                Dictionary<char, bool> txt = new Dictionary<char, bool>();
-                for (var i = 0; i < message.Length; i++)
+                foreach (var symbol in message)
                 {
-                    if (char.IsLetter(message[i]))
-                        if (_messageLetterFrequency.ContainsKey(message[i]))
-                        {
-                            _messageLetterFrequency[message[i]]++;
-                        }
-                        else
-                        {
-                            _messageLetterFrequency.Add(message[i], 1);
-                        }
+                    if (!char.IsLetter(symbol)) continue;
+                    if (MessageLetterFrequency.ContainsKey(symbol))
+                    {
+                        MessageLetterFrequency[symbol]++;
+                    }
+                    else
+                    {
+                        MessageLetterFrequency.Add(symbol, 1);
+                    }
                 }
-
                 var index = 0;
-                var sortedLetters = _messageLetterFrequency.OrderBy(item => item.Value).Reverse();
-                Console.WriteLine();
+                var sortedLetters = MessageLetterFrequency.OrderBy(item => item.Value).Reverse();
                 foreach (var item in sortedLetters)
                 {
-                    Console.WriteLine($"Example: {item.Key}: {item.Value}/Wikipedia: {LetterFrequency[index]}");
-                    //message = message.Replace(item.Key, LetterFrequency[index++]);
+                    Console.WriteLine($"Text: {item.Key} | Wikipedia: {LetterFrequency[index]}");
                     for (var i = 0; i < message.Length; i++)
                     {
                         if (char.IsLetter(message[i]))
@@ -59,20 +56,16 @@ namespace SequenceAnalysis
                     }
                     index++;
                 }
-                message = string.Empty;
-                foreach (var item in decyphered)
-                {
-                    message += item;
-                }
+                message = decyphered.Aggregate(string.Empty, (current, item) => current + item);
                 var objWriter = new StreamWriter(@"C:\Users\rgyrbu\source\repos\SequenceAnalysis\Decyphered.txt", false);
                 objWriter.WriteLine(message);
                 objWriter.Close();
+                Console.WriteLine("Check Decyphered.txt");
                 Console.ReadKey();
-
             }
-            catch (Exception)
+            catch (NullReferenceException)
             {
-                Console.WriteLine("Чёт не то");
+                Console.WriteLine("Текста нет");
                 throw;
             }
         }
